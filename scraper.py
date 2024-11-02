59acf9f2e81e4058823c3ff9f1ca74f068ebcc58
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-11-02 09:21:00 -0500

    trying to figure out how to obtain all the data

--- scraper.py
+++ scraper.py
@@ -1,8 +1,13 @@
-import pandas as pd
 
-print(len(pd.read_html('https://fbref.com/en/squads/19538871/2022-2023/Manchester-United-Stats')))
+# print(len(pd.read_html('https://fbref.com/en/squads/19538871/2022-2023/Manchester-United-Stats')))
 
 
-df = pd.read_html('https://fbref.com/en/squads/19538871/2022-2023/Manchester-United-Stats')[0]
+# df = pd.read_html('https://fbref.com/en/squads/19538871/2022-2023/Manchester-United-Stats')[0]
 
-print(df.columns)
+# print(df.columns)
+
+
+def getData():
+    import pandas as pd
+    df = pd.read_html('https://fbref.com/en/squads/19538871/2022-2023/Manchester-United-Stats')[0]
+    return df

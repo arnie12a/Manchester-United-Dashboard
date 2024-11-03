33dca4849b250b06d4206e971630e4a5a40a836c
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-11-03 12:48:13 -0600

    data that was scraped from website

--- scraper.py
+++ scraper.py
@@ -1,13 +1,24 @@
-
-# print(len(pd.read_html('https://fbref.com/en/squads/19538871/2022-2023/Manchester-United-Stats')))
-
-
-# df = pd.read_html('https://fbref.com/en/squads/19538871/2022-2023/Manchester-United-Stats')[0]
-
-# print(df.columns)
-
-
-def getData():
+def getDataForYear(year):
     import pandas as pd
-    df = pd.read_html('https://fbref.com/en/squads/19538871/2022-2023/Manchester-United-Stats')[0]
-    return df
+    df = pd.read_html(f'https://fbref.com/en/squads/19538871/{str(year)}-{str(year+1)}/Manchester-United-Stats')[0]
+    df.columns = pd.MultiIndex.from_tuples(
+    [('About' if 'Unnamed' in level_0 else level_0, level_1) 
+        for level_0, level_1 in df.columns],
+            names=df.columns.names
+    )
+    df_combined = pd.concat([df['About'], df['Playing Time'], df['Performance']], axis=1)
+    columns_to_keep = ['Player', 'Nation', 'Pos', 'Age', 'MP', 'Min', 'Gls', 'Ast', 'G+A', 'PK']
+    df_combined = df_combined[columns_to_keep]
+    return df_combined
+
+
+def createOutputFolder():
+    import os
+    if not os.path.exists("data"):
+        os.mkdir("data")
+    
+def createCSV(df, year):
+    lowerYear = str(year)
+    upperYear = str(year+1)
+    df.to_csv(f'./data/ManchesterUnited{lowerYear}-{upperYear}.csv', index=False)
+    return 

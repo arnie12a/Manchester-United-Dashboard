558e8081a688cc371e0a19529fd06f96aff554b9
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-11-23 19:22:39 -0600

    added more data

--- scraper.py
+++ scraper.py
@@ -22,3 +22,16 @@ def createCSV(df, year):
     upperYear = str(year+1)
     df.to_csv(f'./data/ManchesterUnited{lowerYear}-{upperYear}.csv', index=False)
     return 
+
+def getDataForYearPremierLeagueTable(year):
+    import pandas as pd
+    df = pd.read_html(f'https://fbref.com/en/comps/9/{str(year)}-{str(year+1)}/{str(year)}-{str(year+1)}-Premier-League-Stats')[0]
+    columns_to_keep = ['Rk', 'Squad', 'MP', 'W', 'L', 'GF', 'GA', 'GD', 'Pts', 'Pts/MP', 'Top Team Scorer', 'Notes']
+    df = df[columns_to_keep]
+    return df
+
+def createCSVPremierLeagueTable(df, year):
+    lowerYear = str(year)
+    upperYear = str(year+1)
+    df.to_csv(f'./data/seasonTeamData/{lowerYear}-{upperYear}.csv', index=False)
+    return 

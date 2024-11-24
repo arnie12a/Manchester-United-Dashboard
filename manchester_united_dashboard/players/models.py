574f91d2d55395b72bed21bf686ea9bb431b9fa0
Arnav Karnik <38790024+arnie12a@users.noreply.github.com>
2024-11-23 20:36:50 -0600

    got rid of django project

--- manchester_united_dashboard/players/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Player(models.Model):
-    name = models.CharField(max_length=100)
-    position = models.CharField(max_length=50)
-    goals = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name
